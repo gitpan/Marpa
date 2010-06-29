a5c2813c771c56fc2389a2cd692aab7d9e390dab
Jeffrey Kegler <jkegl@cpan.org>
2010-06-29 02:52:15 +0000

    Import of JKEGL/Marpa-0.104000 from CPAN.
    gitpan-cpan-distribution: Marpa
    gitpan-cpan-version:      0.104000
    gitpan-cpan-path:         JKEGL/Marpa-0.104000.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     released

--- lib/Marpa.pm
+++ lib/Marpa.pm
@@ -5,7 +5,7 @@ use warnings;
 use strict;
 
 BEGIN {
-    our $VERSION = '0.103_003';
+    our $VERSION = '0.104000';
 }
 
 use Marpa::Internal;
--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -13,11 +13,11 @@ use XSLoader;
 # to load in Marpa as well. Pretty useless otherwise, because we
 # need to _overwrite_ the Marpa methods, we can't have it loading
 # after we do.
-use Marpa 0.103_003 ();
+use Marpa 0.104000 ();
 
 # Define compatibility information
 BEGIN {
-    our $VERSION = '0.103_003';
+    our $VERSION = '0.104000';
     our %EXCLUDE = ();
 }
 
